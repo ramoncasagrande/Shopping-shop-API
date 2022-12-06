e1080b9c92035c40adc5ed304999d4644055f1fa
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-12-06 13:27:51 -0300

    Criado ShopRepository

---
+++ src/main/java/com/shopping/shopapi/repository/ShopRepository.java
@@ -0,0 +1,20 @@
+package com.shopping.shopapi.repository;
+
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shopping.shopapi.model.Shop;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    public List<Shop> findAllByUserIdentifier(String userIdentifier);
+    
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+
+    public List<Shop> findAllByDateGreaterThanEquals(Date date);
+}
