424e8cae40f1668cd602ea4c3f01616ccf57db9a
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-23 17:49:53 -0300

    Corrigido código da migração

--- src/main/resources/db/migration/V1__create_shop_table.sql
+++ src/main/resources/db/migration/V1__create_shop_table.sql
@@ -1,8 +1,8 @@
 create schema if not exists shopping;
 
 create table shopping.shop (
-    id bigserial primary key,u
-    ser_identifier varchar(100) not null,
+    id bigserial primary key,
+    user_identifier varchar(100) not null,
     date timestamp not null,
     total float not null
 );
