dd5009f127238c7384c64cc26e05bf6f4e458368
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-24 10:13:45 -0300

    Implementados endPoints /shopping

--- src/main/java/com/shopping/shopapi/controller/ShopController.java
+++ src/main/java/com/shopping/shopapi/controller/ShopController.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.shopapi.dto.ShopDTO;
 import com.shopping.shopapi.service.ShopService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ShopController {
@@ -16,9 +21,31 @@ public class ShopController {
     private ShopService shopService;
 
     @GetMapping("/shopping")
-    public List<ShopDTO> listaCompras(){
+    public List<ShopDTO> getShops(){
         List<ShopDTO> produtos = shopService.getAll();
         return produtos;
+    }
+
+    @GetMapping("/shopping/shopByUser/{userIdentifier}")
+    public List<ShopDTO> getShops(@PathVariable String userIdentifier){
+        List<ShopDTO> produtos = shopService.getByUser(userIdentifier);
+        return produtos;
+    }
+
+    @GetMapping("/shopping/shopByDate")
+    public List<ShopDTO> getShops(@RequestBody ShopDTO shopDTO){
+        List<ShopDTO> produtos = shopService.getByDate(shopDTO);
+        return produtos;
+    }
+
+    @GetMapping("/shopping/{id}")
+    public ShopDTO findById(@PathVariable Long id){
+        return shopService.findById(id);
+    }
+
+    @PostMapping("/shopping")
+    public ShopDTO newShop(@Valid @RequestBody ShopDTO shopDTO){
+        return shopService.save(shopDTO);
     }
     
 }
--- src/main/java/com/shopping/shopapi/repository/ShopRepository.java
+++ src/main/java/com/shopping/shopapi/repository/ShopRepository.java
@@ -5,11 +5,9 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.shopping.shopapi.model.Shop;
 
-@Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
     public List<Shop> findAllByUserIdentifier(String userIdentifier);
